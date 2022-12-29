9ed83995e8a4d5b08f6a1764d9f086b84afb3fa4
Nathan De Lima <n.de-lima@rgu.ac.uk>
2022-12-29 19:41:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from datetime import datetime
+import uuid
+
+instance_id = uuid.uuid4().hex
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+        
+        return f"Instance ID: {instance_id}"
+        
+        
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
